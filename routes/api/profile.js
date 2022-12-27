ae0c12c891ce7236696d204595b3df3baadd3218
mahmoud magdy <mahmoud.magdy11198@gmail.com>
2022-12-27 23:26:39 +0200

    getting current user profile based on token login adding profile schema

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const auth = require('../../middleware/auth');
 
 
-router.get('/', (req,res) => res.send('Profile route'));
+//get current user profile based on token login
+router.get('/me', auth,async (req,res) => {
+
+    try{
+
+        const profile = await Profile.findOne({ user: req.user.id})
+
+        if(!profile){
+            return res.status(400).json({msg: 'no profile for this user'});
+        }
+        res.json(profile);
+
+    }catch(err){
+        console.error(err.massage);
+        res.status(500).send('server error');
+    }
+
+
+});
 
 
 module.exports = router;
